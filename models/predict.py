4b9222342ac425c9b63567179d02de408cb01100
fedeit <fgalbiati@wpi.edu>
2022-04-26 00:46:11 -0400

    Classifier for WonLost & API

---
+++ models/predict.py
@@ -0,0 +1,15 @@
+from joblib import load
+import pandas as pd
+clf = load('models/WonLostMLPClassifier.joblib') 
+
+def predict(x):
+    x = x.drop(columns=['year', 'competition', 'match', 'mtype', 'bscore', 'rscore', 'red_won', 'blue_won'])
+    y_pred = clf.predict(x)
+    return y_pred
+
+if __name__ == '__main__':    
+    print('Running test __main__')
+    data_test = pd.read_csv('data/v2/test.csv')
+    data_test = data_test.loc[data_test['mtype'].str.contains("qm")]
+    y_pred = predict(data_test)
+    print(y_pred.shape)
