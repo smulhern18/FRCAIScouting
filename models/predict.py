b9afb11d695a5816f1fa2d8ab52a92cfccad5110
fedeit <fgalbiati@wpi.edu>
2022-05-03 13:39:51 -0400

    Code to test model predictions

--- models/predict.py
+++ models/predict.py
@@ -3,13 +3,22 @@ import pandas as pd
 clf = load('models/WonLostMLPClassifier.joblib') 
 
 def predict(x):
-    x = x.drop(columns=['year', 'competition', 'match', 'mtype', 'bscore', 'rscore', 'red_won', 'blue_won'])
+    x = x.drop(columns=['year', 'competition', 'match', 'mtype', 'bscore', 'rscore', 'red_won', 'Unnamed: 0'], errors='ignore')
     y_pred = clf.predict(x)
     return y_pred
 
-if __name__ == '__main__':    
+if __name__ == '__main__':  
+    from sklearn.metrics import accuracy_score  
     print('Running test __main__')
-    data_test = pd.read_csv('data/v2/test.csv')
-    data_test = data_test.loc[data_test['mtype'].str.contains("qm")]
-    y_pred = predict(data_test)
-    print(y_pred.shape)
+    data_train = pd.read_csv('data/v2/train_qm.csv')
+    Y_train = data_train['blue_won'].values
+    X_train = data_train.drop(columns=['blue_won'])
+    y_pred_train = predict(X_train)
+    print('Train', accuracy_score(Y_train, y_pred_train))
+
+    data_test = pd.read_csv('data/v2/test_qm.csv')
+    Y_test = data_test['blue_won'].values
+    X_test = data_test.drop(columns=['blue_won'])
+    y_pred_test = predict(X_test)
+
+    print('Test', accuracy_score(Y_test, y_pred_test))
